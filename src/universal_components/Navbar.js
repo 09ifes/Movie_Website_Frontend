edb21cb52c7b38547fe2fd02491adfe138b488c3
Samuel Ife <samuelife@hotmail.co.uk>
2022-12-16 22:55:23 +0000

    initial commit

---
+++ src/universal_components/Navbar.js
@@ -0,0 +1,15 @@
+
+export default Navbar;
+
+function Navbar(props){
+
+    return (
+        <div className="navbar">
+            <h1 className="logo">Film4View</h1>
+
+
+        </div>
+    )
+
+
+}
