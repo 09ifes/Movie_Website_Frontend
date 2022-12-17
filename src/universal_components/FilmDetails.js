316dbd88780edb8490773de933486e48f665b560
Samuel Ife <samuelife@hotmail.co.uk>
2022-12-17 20:02:30 +0000

    added video to component

--- src/universal_components/FilmDetails.js
+++ src/universal_components/FilmDetails.js
@@ -3,7 +3,11 @@
 
 export default function FilmDetails(props) {
     return (
-        <div>
+        <div id="film-details">
+            
+            <iframe id="film-details-video"
+                src="https://www.youtube.com/embed/tgbNymZ7vqY">
+            </iframe>
 
         </div>
     )
